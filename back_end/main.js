9ea32d227f4a4734e1bce4997c24b76a5dfeb33e
projekt0001i <projekt0001i@gmail.com>
2026-03-04 21:13:06 +0530

    fix cors for production

--- back_end/main.js
+++ back_end/main.js
@@ -6,8 +6,14 @@ require("dotenv").config();
 
 const app = express();
 
-// 🔥 INCREASE BODY SIZE LIMIT (THIS FIXES YOUR ISSUE)
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    process.env.CLIENT_URL
+  ],
+  credentials: true,
+}));
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
